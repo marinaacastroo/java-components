be43d98f88e81c7156971f1a1501f70e3d8572aa
marinaacastroo <marina@marinaacastroo24@gmail.com>
2025-03-13 19:18:37 +0100

    PIOT-GDA-02-002

--- src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
+++ src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
@@ -19,6 +19,9 @@ import programmingtheiot.common.IDataMessageListener;
 import programmingtheiot.common.ResourceNameEnum;
 import programmingtheiot.data.SystemPerformanceData;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 /**
  * Shell representation of class for student implementation.
  * 
@@ -26,18 +29,26 @@ import programmingtheiot.data.SystemPerformanceData;
 public class SystemPerformanceManager
 {
 	// private var's
-	
+	private static final Logger _Logger = Logger.getLogger(SystemPerformanceManager.class.getName());
+
 	
 	// constructors
-	
+	private int pollRate =ConfigConst.DEFAULT_POLL_CYCLES;
+
 	/**
 	 * Default.
 	 * 
 	 */
 	public SystemPerformanceManager()
 	{
+		this.pollRate =
+		ConfigUtil.getInstance().getInteger(
+		ConfigConst.GATEWAY_DEVICE,ConfigConst.POLL_CYCLES_KEY,ConfigConst.DEFAULT_POLL_CYCLES);
+		
+		if (this.pollRate <=0) {
+		this.pollRate =ConfigConst.DEFAULT_POLL_CYCLES;
+			}
 	}
-	
 	
 	// public methods
 	
@@ -49,12 +60,17 @@ public class SystemPerformanceManager
 	{
 	}
 	
-	public void startManager()
+	public boolean startManager()
 	{
+		_Logger.info("SystemPerformanceManager is starting..."); 
+	
+		return true;
 	}
 	
-	public void stopManager()
+	public boolean stopManager()
 	{
+		_Logger.info("SystemPerformanceManager is stopped.");
+        return true;
 	}
 	
 }
